300fe2436f6219eca893add03ee3732a3dd55b71
ramankanabi <ramankanabi@gmail.com>
2024-08-04 11:28:10 +0300

    initialize battery manager channel in native side

--- android/app/src/main/kotlin/com/example/native_platform_channel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/native_platform_channel/MainActivity.kt
@@ -1,5 +1,47 @@
 package com.example.native_platform_channel
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.os.BatteryManager
+import android.content.Context
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+private val CHANNEL = "flutter.battery/methods"
+
+    override fun configureFlutterEngine(
+            flutterEngine: FlutterEngine
+        ) {
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger, 
+            CHANNEL    
+        )
+        .setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != null) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error(
+                        "UNAVAILABLE", 
+                        "Battery level not available.", 
+                        null
+                    )
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int? {
+        val batteryManager = 
+        getSystemService(
+            Context.BATTERY_SERVICE
+        ) as BatteryManager
+        return batteryManager
+        .getIntProperty(
+            BatteryManager.BATTERY_PROPERTY_CAPACITY
+        )
+    }
+}
